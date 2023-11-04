971afe15596756fc587acb243e09b28b7bb82f88
Aryan Rajpoot <araryan1212@gmail.com>
2023-11-04 22:07:53 +0530

    backend

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
 
+const dburl = "mongodb+srv://aryanrajpoot27:Jhansi123@cluster0.setwt3r.mongodb.net/?retryWrites=true&w=majority"
+
+// const connectionParams = {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+  
+// }
+mongoose.connect(dburl).then(() =>{
+    console.log(`connection successful`);
+}).catch ((e) => {
+    console.log(`no connection`);
+})
